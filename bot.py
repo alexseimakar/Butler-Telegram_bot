ccf6acc25a88261603e922609ca6cb9480b92727
Tanckred <makarovaa2001@mail.ru>
2025-02-18 15:16:51 +0300

    /cat + echo_photo
    /cat для рандомной картинки котика + эхо картинок

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
+import requests
 
 # Чтение токена из файла
 def load_token() -> str:
     with open('token.txt', 'r') as file:
         return file.read().strip()
 
+CATS_API_URL = "https://api.thecatapi.com/v1/images/search" #API рандомное фото котика
 BOT_TOKEN = load_token()
 
 
@@ -14,20 +16,32 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 # /start command
-@dp.message(Command(commands=['start']))
 async def process_start_command(message: Message):
     await message.answer("Привет! \nМеня зовут Дух Машины! \nЯ помогаю моему разрабу найти работу и возможно, через пару апдейтов, начну ему помогать с записями идей по тематикам!")
 
 # /help command
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer("Я пока могу только отвечать на help и start, а также передразнивать тебя :Р")
 
+# Эхо фото
+async def send_photo_echo(message: Message):
+    print(message)
+    await message.reply_photo(message.photo[0].file_id)
 # Эхо
-@dp.message()
 async def reply_echo(message: Message):
     await message.reply(text=message.text)
 
+# Рандомный котик
+async def send_cat_APIcall(message: Message):
+    response = requests.get(CATS_API_URL)
+    await message.answer(response.json()[0]["url"])
+
+
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_cat_APIcall, Command(commands='cat'))
+dp.message.register(send_photo_echo, F.photo)
+dp.message.register(reply_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
